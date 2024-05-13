0c1ec96cdf7636f6c2b6fcfed3b52eae9f617fa6
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-05-12 20:08:09 -0500

    feat(frontend): Se añade dropdown User

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -84,14 +84,80 @@ function MainLayout() {
       <section className="dashboard">
         <div className="top bg-main">
           <i
-            className="fa-solid fa-bars fa-xl text-white"
+            className="fa-solid fa-bars fa-2xl text-white"
             onClick={showSidebar}
           ></i>
           <div className="search-box">
             <i className="fa-solid fa-magnifying-glass"></i>
             <input type="text" placeholder="Busca aquí ..." />
           </div>
-          <img src={Avatar} alt="user" />
+          <div className="d-flex justify-content-center align-items-center">
+            <a
+              className="navbar-brand text-white mx-1 header-username"
+              href="#"
+            >
+              Jhon Doe
+            </a>
+            <div className="navbar-nav me-lg-3 mx-lg-4">
+              <div className="flex-shrink-0 dropdown">
+                <a
+                  href="#"
+                  className="d-block link-body-emphasis text-decoration-none dropdown-toggle remove-dropdown-arrow"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src={Avatar}
+                    alt="mdo"
+                    className="rounded-circle header-image-user"
+                  />
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end text-small shadow position-absolute mt-3 header-option custom-dropdown-menu">
+                  <li className="dropdown-item bg-transparent">
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={Avatar}
+                        alt="jhon doe"
+                        className="rounded-2 header-image-user"
+                      />
+                      <div className="ms-3 text-white">
+                        <h5 className="mb-1 fs-sm-5 fs-6">Jhon Doe</h5>
+                        <p className="email-text text-secondary fs-6">
+                          jhondoe@correo.com
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <i className="bi bi-bell-fill"></i> Actividad Reciente
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <i className="bi bi-gear-fill"></i> Ajustes
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <i className="bi bi-person-fill"></i> Perfil
+                    </a>
+                  </li>
+                  <li>
+                    <div className="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <i className="bi bi-door-open-fill"></i> Cerrar Sesión
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="dash-content">
